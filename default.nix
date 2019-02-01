09e6d02cd4cf9b9554df92e0be9552686dd1827d
Michael Bishop <michael.bishop@iohk.io>
2019-02-01 19:45:57 -0400

    force build to use ghc 8.4.4

--- default.nix
+++ default.nix
@@ -1,16 +1,16 @@
-with import <nixpkgs> {};
+{ pkgs }:
 
 let
-  ghc = haskellPackages.ghcWithPackages (ps: with ps; [ aeson-compat servant-server wai warp flow bits-bytestring pipes-wai pipes-bytestring pipes-concurrency pipes-async async pipes cryptonite haxl timeit extra http-client-tls lens ]);
-in stdenv.mkDerivation {
+  ghc = pkgs.haskell.packages.ghc844.ghcWithPackages (ps: with ps; [ aeson-compat servant-server wai warp flow bits-bytestring pipes-wai pipes-bytestring pipes-concurrency pipes-async async pipes cryptonite haxl timeit extra http-client-tls lens ]);
+in pkgs.stdenv.mkDerivation {
   name = "cachecache";
-  buildInputs = [ ghc ] ++ (with haskellPackages; [ stylish-haskell ghcid hlint ]);
+  buildInputs = [ ghc ] ++ (with pkgs.haskellPackages; [ stylish-haskell ghcid hlint ]);
   shellHook = ''
     buildAll() {
       ghc cachecache.hs -o cachecache -Wall -threaded -rtsopts
     }
   '';
-  src = lib.cleanSource ./.;
+  src = pkgs.lib.cleanSource ./.;
   installPhase = ''
     mkdir -p $out/bin/
     ghc -Wall -threaded ./cachecache.hs -o $out/bin/cachecache
